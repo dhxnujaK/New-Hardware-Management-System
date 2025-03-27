4af09b15df8f3f2d1db518e2de202cbabb29936d
Dhanuja Kahatapitiya <D.P.kahatapitiya@gmail.com>
2025-03-27 22:19:13 +0530

    item class

--- Models/item.cs
+++ Models/item.cs
@@ -8,6 +8,12 @@ namespace New_Hardware_Management_System.Models
 {
     internal class item
     {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public double BuyingPrice { get; set; }
+        public double SellingPrice { get; set; }
 
     }
 }
